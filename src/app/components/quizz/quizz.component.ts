283097a937c9dd6edd300d858c7e7038280a5c9b
Suellen <suellen.diass@yahoo.com.br>
2022-12-25 10:18:06 -0300

    second commit

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css']
+})
+
+export class QuizzComponent implements OnInit {
+
+  title:string = ""
+
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string =""
+
+ //inicia a questão do zero, ponteiro inicial e maximo
+  questionIndex:number =0
+  questionMaxIndex:number=0
+
+  // quando terminar de preencher
+  finished:boolean = false
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      console.log(this.questionIndex)
+      console.log(this.questionMaxIndex)
+    }
+
+  }
+
+  playerChoose(value:string){
+    this.answers.push(value)
+    this.nextStep()
+
+  }
+ // próxima questão e exibir resultado
+  async nextStep(){
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex){
+        this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished = true
+// verificar opção ganhadora
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results ]
+    }
+  }
+//checa resultado
+  async checkResult(anwsers:string[]){
+
+    const result = anwsers.reduce((previous, current, i, arr)=>{
+        if(
+          arr.filter(item => item === previous).length >
+          arr.filter(item => item === current).length
+        ){
+          return previous
+        }else{
+          return current
+        }
+    })
+
+    return result
+  }
+
+}
